64c43c2a5533db0b14fa4609992cbc54dd6c0a1c
Amandine Cian <amandine.cian@gmail.com>
2023-07-21 15:54:46 +0200

    article by id & delete by id

--- src/main/java/fr/maboite/correction/rest/controller/ArticleController.java
+++ src/main/java/fr/maboite/correction/rest/controller/ArticleController.java
@@ -1,7 +1,9 @@
 package fr.maboite.correction.rest.controller;
 
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 
 @Path("/articles")
 public class ArticleController {
@@ -10,4 +12,18 @@ public class ArticleController {
 		System.out.println("méthode 1 fonctionne ?");
 		return "youhou je suis là";
 	}
+	
+	@GET
+	@Path("/{id}")
+	public int articleById(@PathParam("id") int id) {
+		System.out.println(id + " trop fort");
+		return id;
+	 }
+	
+	@DELETE
+	@Path("/{id}")
+	public int deleteArticleById(@PathParam("id") int id) {
+		System.out.println(id + " aurevoir");
+		return id;
+	 }
 }
