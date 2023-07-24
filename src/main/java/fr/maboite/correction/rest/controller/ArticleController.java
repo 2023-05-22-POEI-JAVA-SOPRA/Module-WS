deabba6b2013ab65723f8f09d71e24f5322d11d2
Laurent Plessis <Laurent.plessis@gmail.com>
2023-07-24 13:36:56 +0200

    Modification de la matinée

--- src/main/java/fr/maboite/correction/rest/controller/ArticleController.java
+++ src/main/java/fr/maboite/correction/rest/controller/ArticleController.java
@@ -2,14 +2,20 @@ package fr.maboite.correction.rest.controller;
 
 import entity.Article;
 import entity.ArticlePojo;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.ResponseBuilder;
 
 
 @Path("/articles")
+@Produces(MediaType.APPLICATION_JSON)
 public class ArticleController {
 
 	public ArticleController() {
@@ -32,15 +38,41 @@ public class ArticleController {
 //		return idArticle;
 //	}
 	
+//	@GET
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	@Produces(MediaType.APPLICATION_JSON)
+//	@Path("/{idArticle}")
+//	public ArticlePojo getArticlePojo(@PathParam("idArticle") Integer idArticle){
+//		ArticlePojo monPojo=new ArticlePojo();
+//		monPojo.setId(idArticle);
+//		monPojo.setName("maBelPojo309");
+//		System.out.println("Test methode getArticlePojo - objet ArticlePojo : "+monPojo);
+//		return monPojo;
+//	}
+	
 	@GET
-	@Produces("application/json")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/{idArticle}")
-	public ArticlePojo getArticlePojo(@PathParam("idArticle") Integer idArticle){
+	public Response getArticlePojo(@PathParam("idArticle") Integer idArticle){
+		if(idArticle<0) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
 		ArticlePojo monPojo=new ArticlePojo();
 		monPojo.setId(idArticle);
 		monPojo.setName("maBelPojo309");
 		System.out.println("Test methode getArticlePojo - objet ArticlePojo : "+monPojo);
-		return monPojo;
+		return Response.ok(monPojo).build();
+	}
+	
+	@GET
+	@Produces("text/plain")
+	@Path("/blablabla")
+	public String getArticlePojoPlainText(){
+		ArticlePojo monPojo=new ArticlePojo(12,"maBelPojo309");
+		System.out.println("PLAIN TEXT getArticlePojoPlainText - objet ArticlePojo : "+monPojo);
+		System.out.println(monPojo);
+		return monPojo.toString();
 	}
 	
 	
@@ -50,4 +82,16 @@ public class ArticleController {
 		System.out.println("Test methode methodeDelete - id : "+babibu);
 		return babibu;
 	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public ArticlePojo maMethodePost(ArticlePojo a) {
+		System.out.println("L'article "+a.getId()+" est passe par POST");
+		
+		a.setName("Mon ArticlePojo Modifié");
+		a.setId(a.getId()+12);
+		System.out.println(a);
+		return a;
+	}
+	
 }
