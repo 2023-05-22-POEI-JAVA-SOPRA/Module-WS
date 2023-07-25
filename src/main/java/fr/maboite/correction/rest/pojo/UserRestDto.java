2c70d7313c0e4d8916e34ea5a3021c230088c379
Nicolas Postaire <postairenicolas2501@gmail.com>
2023-07-25 15:27:15 +0200

    ajout userrestdto

--- src/main/java/fr/maboite/correction/rest/pojo/UserRestDto.java
+++ src/main/java/fr/maboite/correction/rest/pojo/UserRestDto.java
@@ -1,5 +1,69 @@
 package fr.maboite.correction.rest.pojo;
 
-public class UserRestDto {
 
+public class UserRestDto {
+	private Integer idUser;
+	private String login;
+	private String password;
+	private Integer connectionNumber;
+	
+	
+	public UserRestDto() {
+		super();
+	}
+
+
+	public UserRestDto(String login, String password, Integer connectionNumber) {
+		super();
+		this.login = login;
+		this.password = password;
+		this.connectionNumber = connectionNumber;
+	}
+
+
+	public Integer getIdUser() {
+		return idUser;
+	}
+
+
+	public String getLogin() {
+		return login;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public Integer getConnectionNumber() {
+		return connectionNumber;
+	}
+
+
+	public void setIdUser(Integer idUser) {
+		this.idUser = idUser;
+	}
+
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public void setConnectionNumber(Integer connectionNumber) {
+		this.connectionNumber = connectionNumber;
+	}
+
+
+	@Override
+	public String toString() {
+		return "UserRestDto [idUser=" + idUser + ", login=" + login + ", password=" + password + ", connectionNumber="
+				+ connectionNumber + "]\n";
+	}
 }
