74f404a9b85b685512efa34997a28b1b84d02164
Nicolas Postaire <postairenicolas2501@gmail.com>
2023-07-26 17:17:54 +0200

    ajout relation commandline command

--- src/main/java/fr/maboite/correction/jpa/entity/Command.java
+++ src/main/java/fr/maboite/correction/jpa/entity/Command.java
@@ -1,10 +1,11 @@
 package fr.maboite.correction.jpa.entity;
 
 import java.sql.Date;
+import java.util.List;
 
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -20,7 +21,8 @@ public class Command {
 	private User user;
 	@Column(name = "command_date")
 	private Date commandDate;
-	
+	@OneToMany(mappedBy = "command")
+	private List<CommandLine> commandLines;
 	
 	public Command() {
 		super();
