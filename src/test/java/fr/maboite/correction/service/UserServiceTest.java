4fe51f68ffd93f20068c1e94e47ff425229daaa3
Nicolas Postaire <postairenicolas2501@gmail.com>
2023-07-26 12:00:49 +0200

    modif test

--- src/test/java/fr/maboite/correction/service/UserServiceTest.java
+++ src/test/java/fr/maboite/correction/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package fr.maboite.correction.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -34,6 +35,8 @@ public class UserServiceTest {
 	public void delete_user_with_non_existing_id() {
 		assertEquals(service.delete(1000000000), false);
 	}
+	
+	//Create================================================================================================//
 	
 	@Test
 	public void create_user_with_null_login() {
@@ -69,5 +72,51 @@ public class UserServiceTest {
 	public void create_valid_user() {
 		User u = new User("test@test.com", "testtest", 5162651);
 		assertTrue(service.create(u));
+	}
+	
+	//Update================================================================================================//
+
+	
+	
+	@Test
+	public void update_user_with_non_existing_id() {
+		User u = new User("aaa@bb.com", "123456", 5162651);
+		assertFalse(service.update(u, 100000000));
+	}
+	
+	@Test
+	public void update_user_with_null_login() {
+		User u = new User(null, "123456", 5162651);
+		assertFalse(service.update(u, 2));
+	}
+	
+	@Test
+	public void update_user_with_null_password() {
+		User u = new User("aaa@bb.com", null, 5162651);
+		assertFalse(service.update(u, 2));
+	}
+	
+	@Test
+	public void update_user_with_null_connectionNumber() {
+		User u = new User("aaa@bb.com", "123456", null);
+		assertFalse(service.update(u, 2));
+	}
+
+	@Test
+	public void update_user_with_login_size_sup_to_20() {
+		User u = new User("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa@bb.com", "12345678", 5162651);
+		assertFalse(service.update(u, 2));
+	}
+	
+	@Test
+	public void update_user_with_password_size_sup_to_20() {
+		User u = new User("aaaaa@bb.com", "12345678888888888888888888888888888888888888888", 5162651);
+		assertFalse(service.update(u, 2));
+	}
+	
+	@Test
+	public void update_valid_user() {
+		User u = new User("test@test.com", "testtest", 5162651);
+		assertTrue(service.update(u , 3));
 	}
 }
