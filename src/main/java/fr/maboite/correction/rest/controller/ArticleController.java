ea5bf725ac0f8661ebf0df1ed4d7a0d192b9bf22
Julie Thomas <ladyjulie63@gmail.com>
2023-07-26 14:42:05 +0200

    Still fix

--- src/main/java/fr/maboite/correction/rest/controller/ArticleController.java
+++ src/main/java/fr/maboite/correction/rest/controller/ArticleController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package fr.maboite.correction.rest.controller;
 
 import java.util.ArrayList;
@@ -65,74 +64,5 @@ public class ArticleController {
 		System.out.println("delete called with Id : "+id);
 		this.articleService.delete(id);
 	}
-
-=======
-package fr.maboite.correction.rest.controller;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
-import fr.maboite.correction.jpa.entity.Article;
-import fr.maboite.correction.rest.pojo.ArticlePojo;
-import fr.maboite.correction.rest.pojo.ArticleRestDto;
-import fr.maboite.correction.service.ArticleService;
-import jakarta.validation.Valid;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.DELETE;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-
-@Path("/articles")
-@Produces(MediaType.APPLICATION_JSON)
-public class ArticleController {
 	
-	private ArticleService articleService = new ArticleService();
-	
-	
-	@GET
-	public List<ArticleRestDto> getArticles() {
-		System.out.println("getArticles called");
-		List<Article> articles = this.articleService.findAll();
-		List<ArticleRestDto> articleDtos = new ArrayList<ArticleRestDto>();
-		for (Article article:articles) {
-			ArticleRestDto articleDto = new ArticleRestDto(article);
-			articleDtos.add(articleDto);
-		}
-		return articleDtos;
-	}
-	
-	@GET
-	@Path("/{idArticle}")
-	public ArticleRestDto getArticles(@PathParam("idArticle") Integer id) {
-		System.out.println("getById called with Id : "+id);
-		Article article = this.articleService.get(id);
-		if(article==null) {
-			return null;
-		}
-		return new ArticleRestDto(article);
-	}
-	
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	public void save(@Valid ArticleRestDto articlePojo) {
-		System.out.println("save is called");
-		if(articleService.checkPrice(articlePojo)) {
-			this.articleService.save(articlePojo.toArticle());
-		}
-	}
-
-	@DELETE
-	@Path("/{idArticle}")
-	public void deleteArticles(@PathParam("idArticle") Integer id) {
-		System.out.println("delete called with Id : "+id);
-		this.articleService.delete(id);
-	}
-
->>>>>>> b6562d84c546239d3ddb946257ccb09743b6b5a6
 }
--- src/main/java/fr/maboite/correction/service/ArticleService.java
+++ src/main/java/fr/maboite/correction/service/ArticleService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package fr.maboite.correction.service;
 
 import java.util.List;
@@ -61,67 +60,3 @@ public class ArticleService {
 	}
 	
 }
-=======
-package fr.maboite.correction.service;
-
-import java.util.List;
-
-import fr.maboite.correction.jpa.dao.ArticleDao;
-import fr.maboite.correction.jpa.entity.Article;
-import fr.maboite.correction.rest.pojo.ArticleRestDto;
-
-public class ArticleService {
-
-	private ArticleDao articleDao = new ArticleDao();
-
-	/**
-	 * Sauvegarde (insère ou met à jour) article.
-	 * 
-	 * @param article
-	 * @return
-	 */
-	public Article save(Article article) {
-		return this.articleDao.save(article);
-	}
-
-	/**
-	 * Renvoie le Article ayant id, peut renvoyer null.
-	 * 
-	 * @param id
-	 * @return
-	 */
-	public Article get(Integer id) {
-		return this.articleDao.get(id);
-	}
-
-	/**
-	 * Supprime l'article avec l'id id
-	 * 
-	 * @param id
-	 */
-	public void delete(Integer id) {
-		this.articleDao.delete(id);
-	}
-
-	/**
-	 * Renvoie tous les Users en base de données.
-	 * 
-	 * @return
-	 */
-	public List<Article> findAll() {
-		return this.articleDao.findAll();
-	}
-	
-	public boolean checkPrice(ArticleRestDto articleRestDto) {
-		String brand = articleRestDto.getBrand();
-		double unitaryPrice = articleRestDto.getUnitaryPrice();
-		if(unitaryPrice>500) {
-			if (!(brand.equals("Rolex") || brand.equals("Ferrari")) || brand.equals("Marque repère")) {
-				return false;
-			}
-		}
-		return true;
-	}
-	
-}
->>>>>>> b6562d84c546239d3ddb946257ccb09743b6b5a6
